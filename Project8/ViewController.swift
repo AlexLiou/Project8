baefe0dc1d2dd1e9a776bd7ea73a15d48baccee5
alexliou <alexliou0@gmail.com>
2022-07-24 14:48:27 -0400

    Completed challenges for Project 8

--- Project8/ViewController.swift
+++ Project8/ViewController.swift
@@ -47,12 +47,20 @@ class ViewController: UIViewController {
         let submit = UIButton(type: .system)
         submit.translatesAutoresizingMaskIntoConstraints = false
         submit.setTitle("SUBMIT", for: .normal)
+        submit.addTarget(self, action: #selector(submitTapped), for: .touchUpInside)
+        submit.layer.borderWidth = 1
+        submit.layer.borderColor = UIColor.lightGray.cgColor
+        submit.layer.cornerRadius = 5
         return submit
     }()
     lazy var clear: UIButton = { [weak self] in
         let clear = UIButton(type: .system)
         clear.translatesAutoresizingMaskIntoConstraints = false
         clear.setTitle("CLEAR", for: .normal)
+        clear.addTarget(self, action: #selector(clearTapped), for: .touchUpInside)
+        clear.layer.borderWidth = 1
+        clear.layer.borderColor = UIColor.lightGray.cgColor
+        clear.layer.cornerRadius = 5
         return clear
     }()
     lazy var buttonsView: UIView = { [weak self] in
@@ -61,6 +69,16 @@ class ViewController: UIViewController {
         return buttonsView
     }()
     var letterButtons = [UIButton]()
+    var activatedButtons = [UIButton]()
+    var solutions = [String]()
+    var correctAnswers = 0
+
+    var score = 0 {
+        didSet {
+            scoreLabel.text = "Score: \(score)"
+        }
+    }
+    var level = 1
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -121,9 +139,10 @@ class ViewController: UIViewController {
 
         createButtonGrid()
 
-        buttonsView.backgroundColor = .green
-        cluesLabel.backgroundColor = .red
-        answersLabel.backgroundColor = .blue
+//        buttonsView.backgroundColor = .green
+//        cluesLabel.backgroundColor = .red
+//        answersLabel.backgroundColor = .blue
+        loadLevel()
     }
 
     func createButtonGrid() {
@@ -146,7 +165,114 @@ class ViewController: UIViewController {
                 buttonsView.addSubview(letterButton)
                 // and also to our letterButtons array
                 letterButtons.append(letterButton)
+                letterButton.addTarget(self, action: #selector(letterTapped), for: .touchUpInside)
+
+                // give buttons a light gray border
+                letterButton.layer.borderWidth = 1
+                letterButton.layer.borderColor = UIColor.lightGray.cgColor
+                letterButton.layer.cornerRadius = 5
             }
+        }
+    }
+
+    @objc func letterTapped(_ sender: UIButton) {
+        guard let buttonTitle = sender.titleLabel?.text else { return }
+        currentAnswer.text = currentAnswer.text?.appending(buttonTitle)
+        activatedButtons.append(sender)
+        sender.isHidden = true
+    }
+
+    @objc func submitTapped(_ sender: UIButton) {
+        guard let answerText = currentAnswer.text else { return }
+
+        if let solutionPosition = solutions.firstIndex(of: answerText) {
+            activatedButtons.removeAll()
+
+            var splitAnswers = answersLabel.text?.components(separatedBy: "\n")
+            splitAnswers?[solutionPosition] = answerText
+            answersLabel.text = splitAnswers?.joined(separator: "\n")
+
+            currentAnswer.text = ""
+            score += 1
+            correctAnswers+=1
+
+            checkCompleted()
+        } else {
+            let ac = UIAlertController(title: "Wrong Answer!", message: "You gotta think outside the box!", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "Try Again", style: .default))
+            present(ac, animated: true)
+            if score > 0 {
+                score -= 1
+            }
+        }
+    }
+
+    @objc func clearTapped(_ sender: UIButton) {
+        currentAnswer.text = ""
+
+        for btn in activatedButtons {
+            btn.isHidden = false
+        }
+
+        activatedButtons.removeAll()
+    }
+
+    func checkCompleted() {
+        if correctAnswers == 7 {
+            let ac = UIAlertController(title: "Well done!", message: "Are you ready for the next level?", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "Let's go!", style: .default, handler: levelUp))
+            present(ac, animated: true)
+        }
+    }
+
+    func loadLevel() {
+        var clueString = ""
+        var solutionString = ""
+        var letterBits = [String]()
+
+        if let levelFileURL = Bundle.main.url(forResource: "level\(level)", withExtension: "txt") {
+            if let levelContents = try? String(contentsOf: levelFileURL) {
+                var lines = levelContents.components(separatedBy: "\n")
+                lines.shuffle()
+
+                for (index, line) in lines.enumerated() {
+                    let parts = line.components(separatedBy: ": ")
+                    let answer = parts[0]
+                    let clue = parts[1]
+
+                    clueString += "\(index + 1). \(clue)\n"
+
+                    let solutionWord = answer.replacingOccurrences(of: "|", with: "")
+                    solutionString += "\(solutionWord.count) letters\n"
+                    solutions.append(solutionWord)
+
+                    let bits = answer.components(separatedBy: "|")
+                    letterBits += bits
+                }
+            }
+        }
+
+        cluesLabel.text = clueString.trimmingCharacters(in: .whitespacesAndNewlines)
+        answersLabel.text = solutionString.trimmingCharacters(in: .whitespacesAndNewlines)
+
+        letterBits.shuffle()
+
+        if letterBits.count == letterButtons.count {
+            for i in 0 ..< letterButtons.count {
+                letterButtons[i].setTitle(letterBits[i], for: .normal)
+            }
+        }
+    }
+
+    func levelUp(action: UIAlertAction) {
+        correctAnswers = 0
+        level += 1
+        solutions.removeAll(keepingCapacity: true)
+
+        loadLevel()
+
+        for btn in letterButtons {
+            btn.isHidden = false
         }
     }
 
